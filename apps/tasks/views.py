56f6126dd14e2742e1863f36944273908f8c42c5
Азат <zachet04@gmail.com>
2025-09-29 16:37:01 +0500

    feat: add views and templates for home, users, city time, and CRN functionalities

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -33,3 +33,115 @@ def hello_view(
         context={"name": "Azat", "names": ['Azat', 'Berta', 'John']},
         status=200
     )
+
+def home_view(
+    request: HttpRequest,
+    *args: tuple[Any, ...],
+    **kwargs: dict[str, Any]
+) -> HttpResponse:
+    """
+    Return a simple HTML page.
+
+    Parameters:
+        request: HttpRequest
+            The request object.
+        *args: list
+            Additional positional arguments.
+        **kwargs: dict
+            Additional keyword arguments.
+    
+    Returns:
+        HttpResponse
+            Rendered HTML page with a name in the context.
+    """
+
+    return render(
+        request=request,
+        template_name="home.html",
+        context={},
+        status=200
+    )
+
+def users_view(
+    request: HttpRequest,
+    *args: tuple[Any, ...],
+    **kwargs: dict[str, Any]
+) -> HttpResponse:
+    """
+    Return a simple HTML page.
+
+    Parameters:
+        request: HttpRequest
+            The request object.
+        *args: list
+            Additional positional arguments.
+        **kwargs: dict
+            Additional keyword arguments.
+    
+    Returns:
+        HttpResponse
+            Rendered HTML page with a name in the context.
+    """
+
+    return render(
+        request=request,
+        template_name="users.html",
+        context={'users': ['Azat', 'Berta', 'John']},
+        status=200
+    )
+
+def city_time_view(
+    request: HttpRequest,
+    *args: tuple[Any, ...],
+    **kwargs: dict[str, Any]
+) -> HttpResponse:
+    """
+    Return a simple HTML page.
+
+    Parameters:
+        request: HttpRequest
+            The request object.
+        *args: list
+            Additional positional arguments.
+        **kwargs: dict
+            Additional keyword arguments.
+    
+    Returns:
+        HttpResponse
+            Rendered HTML page with a name in the context.
+    """
+
+    return render(
+        request=request,
+        template_name="city-time.html",
+        context={},
+        status=200
+    )
+
+def crn_view(
+    request: HttpRequest,
+    *args: tuple[Any, ...],
+    **kwargs: dict[str, Any]
+) -> HttpResponse:
+    """
+    Return a simple HTML page.
+
+    Parameters:
+        request: HttpRequest
+            The request object.
+        *args: list
+            Additional positional arguments.
+        **kwargs: dict
+            Additional keyword arguments.
+    
+    Returns:
+        HttpResponse
+            Rendered HTML page with a name in the context.
+    """
+
+    return render(
+        request=request,
+        template_name="crn.html",
+        context={},
+        status=200
+    )
--- settings/urls.py
+++ settings/urls.py
@@ -4,8 +4,16 @@ from django.urls import path
 
 # Project modules
 from apps.tasks.views import hello_view
+from apps.tasks.views import home_view
+from apps.tasks.views import users_view
+from apps.tasks.views import city_time_view
+from apps.tasks.views import crn_view
 
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path(route="hello/", view=hello_view, name="hello-view"),
+    path(route="users/", view=users_view, name="users-view"),
+    path(route="city-time/", view=city_time_view, name="city-time-view"),
+    path(route="crn/", view=crn_view, name="crn-view"),
 ]
