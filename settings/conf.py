960874d885a68b1b23c00ad417f2ae3c38aa9dc1
Азат <zachet04@gmail.com>
2025-09-29 15:55:10 +0500

    feat: add environment configuration for DJANGORLAR_ENV_ID and possible options

--- settings/conf.py
+++ settings/conf.py
@@ -1,1 +1,9 @@
+from decouple import config
+
+
+ENV_POSSIBLE_OPTIONS = (
+    "local",
+    "prod",
+)
+ENV_ID = config("DJANGORLAR_ENV_ID", cast=str)
 SECRET_KEY = 'django-insecure-4n(vsk0xjz7kw%7qk1q2h&n8d2q$cwo4%&_dc$%3^ka96t6#x0'
